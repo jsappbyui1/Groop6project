ad4d5f5142b60286a624b7d6f2f2ab73d8e3cb7c
jsappbyui1 <sap19001@byui.edu>
2020-12-02 09:36:02 -0700

    Badguy in top left, pointing at player

--- Void_Shooter.py
+++ Void_Shooter.py
@@ -2,6 +2,7 @@
 
 import random
 import arcade
+import math
 
 # Constants
 SCREEN_WIDTH = 1000
@@ -10,10 +11,12 @@ SCREEN_TITLE = "Void Shooter"
 
 MOVEMENT_SPEED = 5
 
-
 class Game (arcade.Window):
 
     def __init__(self):
+
+        self.frame_count = 0
+
         # Call parent class and set up the window, probably from the arcade library.
         super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
         
@@ -22,11 +25,11 @@ class Game (arcade.Window):
 
         # variables that hold sprite lists initialization
         self.player_list = None
-        self.bullet_list = None
         self.boss_list = None
         
         # player info initialization
         self.player_sprite = None
+        self.boss_sprite = None
 
         # Track current state of what key is pressed
         self.left_pressed = False
@@ -40,11 +43,19 @@ class Game (arcade.Window):
     def setup(self):
         """Call this function to restart the game."""
         self.player_list = arcade.SpriteList()
+        self.enemy_list = arcade.SpriteList()
 
         self.player_sprite = arcade.Sprite(":resources:images/space_shooter/playerShip3_orange.png")
         self.player_sprite.center_x = 500
         self.player_sprite.center_y = 500
         self.player_list.append(self.player_sprite)
+
+        # Add top-left enemy ship
+        enemy = arcade.Sprite(":resources:images/space_shooter/playerShip1_green.png", 0.5)
+        enemy.center_x = 120
+        enemy.center_y = SCREEN_HEIGHT - enemy.height
+        enemy.angle = 180
+        self.enemy_list.append(enemy)
 
     def on_draw(self):
         """Render the Screen"""
@@ -52,11 +63,15 @@ class Game (arcade.Window):
         arcade.start_render()
        
         # Draw all the sprites
+        self.enemy_list.draw()
+        
         self.player_list.draw()
+
 
     def on_update(self, detla_time):
         """Movement and game logic"""
         # Calculate speed based on keys pressed
+        self.frame_count += 1
         self.player_sprite.change_x = 0
         self.player_sprite.change_y = 0
 
@@ -72,6 +87,32 @@ class Game (arcade.Window):
         
         # Call update to move the sprite
         self.player_list.update()
+
+        for enemy in self.enemy_list:
+
+            # First, calculate the angle to the player. We could do this
+            # only when the bullet fires, but in this case we will rotate
+            # the enemy to face the player each frame, so we'll do this
+            # each frame.
+
+            # Position the start at the enemy's current location
+            start_x = enemy.center_x
+            start_y = enemy.center_y
+
+            # Get the destination location for the bullet
+            dest_x = self.player_sprite.center_x
+            dest_y = self.player_sprite.center_y
+
+            # Do math to calculate how to get the bullet to the destination.
+            # Calculation the angle in radians between the start points
+            # and end points. This is the angle the bullet will travel.
+            x_diff = dest_x - start_x
+            y_diff = dest_y - start_y
+            angle = math.atan2(y_diff, x_diff)
+
+            # Set the enemy to face the player.
+            enemy.angle = math.degrees(angle)-90
+
 
     def on_key_press(self, key, modifiers):
         """Called whenever a key is pressed"""
@@ -116,8 +157,6 @@ class Player(arcade.Sprite):
             self.top  =SCREEN_HEIGHT - 1
 
 
-class BadguySprite(arcade.Sprite):
-    pass
 
 
 def main():
