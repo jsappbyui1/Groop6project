a9843f37e199c7b44d0d9e2fe5b16af4995d70b1
Sean Kirkland <73958885+conconcraft-py@users.noreply.github.com>
2020-12-07 19:26:32 -0700

    Update Bullets.py

--- Bullets.py
+++ Bullets.py
@@ -1,70 +1,45 @@
 
-class Bullet():
-    """ Bullet """
+""" Class Bullet"""
+class Bullet(arcade.Sprite):
+
     def __init__(self):
         self.bullet_list = None
 
-    def create_player_bullet(start_x, start_y, end_x, end_y):
-        """ Called whenever the mouse button is clicked. """
-        mygame = MyGame()
+    def setup(self):
+        self.bullet_list = arcade.SpriteList()
 
-        # Create a bullet
-        bullet = arcade.Sprite(":resources:images/space_shooter/laserBlue01.png", SPRITE_SCALING_LASER)
-
-        # Set Owner
-        
-
-        # Position the bullet at the player's current location
+    def createBullet(self,start_x,start_y,angle):
+        bullet = arcade.Sprite(":resources:images/space_shooter/laserBlue01.png")
         bullet.center_x = start_x
         bullet.center_y = start_y
-
-        # Get from the mouse the destination location for the bullet
-        # IMPORTANT! If you have a scrolling screen, you will also need
-        # to add in self.view_bottom and self.view_left.
-        dest_x = end_x
-        dest_y = end_y
-
-        # Do math to calculate how to get the bullet to the destination.
-        # Calculation the angle in radians between the start points
-        # and end points. This is the angle the bullet will travel.
-        x_diff = dest_x - start_x
-        y_diff = dest_y - start_y
-        angle = math.atan2(y_diff, x_diff)
-
-        # Angle the bullet sprite so it doesn't look like it is flying
-        # sideways.
+        # Angle the bullet sprite
         bullet.angle = math.degrees(angle)
-        print(f"Bullet angle: {bullet.angle:.2f}")
 
         # Taking into account the angle, calculate our change_x
         # and change_y. Velocity is how fast the bullet travels.
         bullet.change_x = math.cos(angle) * BULLET_SPEED
         bullet.change_y = math.sin(angle) * BULLET_SPEED
+        
+        self.bullet_list.append(bullet)
 
-        # Add the bullet to the appropriate lists
-        mygame.bullet_list.append(bullet)
-
-    def on_update(self, delta_time):
-        """ Movement and game logic """
-        mygame = MyGame()
-
-        # Call update on all sprites
-        mygame.bullet_list.update()
-
-        # Loop through each bullet
-        for bullet in mygame.bullet_list:
-
-            # Check this bullet to see if it hit a coin
-            hit_list = arcade.check_for_collision_with_list(bullet, mygame.boss)
+    def update(self,target):
+        # Get rid of the bullet when it flies off-screen
+        for bullet in self.bullet_list:
+            if bullet.top < 0:
+                bullet.remove_from_sprite_lists()
+                        # Check this bullet to see if it hit a coin
+            hit_list = arcade.check_for_collision_with_list(bullet, target)
 
             # If it did, get rid of the bullet
             if len(hit_list) > 0:
                 bullet.remove_from_sprite_lists()
-
-            # For every hit, add to the score
-            for coin in hit_list:
-                self.score += 1
+            #self.score += 1 # <-- For every coin we hit, add to the score and remove the coin
 
             # If the bullet flies off-screen, remove it.
-            if bullet.bottom > mygame.width or bullet.top < 0 or bullet.right < 0 or bullet.left > mygame.width:
+            if bullet.bottom > SCREEN_HEIGHT:
                 bullet.remove_from_sprite_lists()
+
+        self.bullet_list.update()
+    
+    def draw(self):
+        self.bullet_list.draw()
